b1f10f30c34a736101197a65a04408f6c5ca4a8e
MeganLDeans <megan.deans@snhu.edu>
2022-12-08 13:43:22 -0500

    Initial Commit of Blog Site code

---
+++ portfolio/models.py
@@ -0,0 +1,11 @@
+
+from django.db import models
+
+class Portfolio(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(
+        "auth.User",
+        on_delete=models.CASCADE,
+    )
+    body = models.TextField()
+# Create your models here.
