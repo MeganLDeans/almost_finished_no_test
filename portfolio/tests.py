29cfeec3a2f5ffd0443db6f63d7c30c9403290e5
MeganLDeans <megan.deans@snhu.edu>
2022-12-15 00:10:11 -0500

    12.10 EVERYTHING WORKS need to the test

--- portfolio/tests.py
+++ portfolio/tests.py
@@ -1,3 +1,15 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+from .models import Portfolio
+
+class PortfolioTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = get_user_model().objects.create_user(
+            username="testuser", email="test@email.com", password="secret"
+        )
+        cls.post = Portfolio.objects.create(
+            caption="A good title",
+        )
