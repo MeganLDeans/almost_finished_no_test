e17212a9da7ce18cf000709534095f63784f8d3f
MeganLDeans <megan.deans@snhu.edu>
2022-12-14 20:48:09 -0500

    chpt 6 blog update

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -2,11 +2,9 @@
 __author__ = "Megan Deans"
 from django.urls import path
 from .views import image_request
-from .views import PortfolioListView
+from .views import PortfolioListView, PortfolioDetailView
 urlpatterns = [
+    path("portfolio/<int:pk>/", PortfolioDetailView.as_view(), name="portfolio_detail"),
     path("", PortfolioListView.as_view(), name="home"),
-]
-
-urlpatterns = [
     path("", image_request, name="image-request")
 ]
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,15 +1,14 @@
 from django.shortcuts import redirect, render
 from .forms import UserImage
 from .models import Portfolio
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
+
 
 def image_request(request):
     if request.method == 'POST':
         form = UserImage(request.POST, request.FILES)
         if form.is_valid():
             form.save()
-
-            # Getting the current instance object to display in the template
             img_object = form.instance
 
             return render(request, 'image_form.html', {'form': form, 'img_obj': img_object})
@@ -18,6 +17,12 @@ def image_request(request):
 
     return render(request, 'image_form.html', {'form': form})
 
+
 class PortfolioListView(ListView):
     model = Portfolio
     template_name = "home.html"
+
+
+class PortfolioDetailView(DetailView):
+    model = Portfolio
+    template_name = "portfolio_detail.html"
